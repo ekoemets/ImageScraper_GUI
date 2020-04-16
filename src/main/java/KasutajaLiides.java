3d8d07072ce72041440d0b241921f04fde77424f
emilkoemets <emilkoemets@gmail.com>
2020-04-16 14:21:58 +0300

    Gui main application and additional css

---
+++ src/main/java/KasutajaLiides.java
@@ -0,0 +1,59 @@
+import ImageScraper.VeebiKlient;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+
+public class KasutajaLiides extends Application {
+
+    private final static int PADDING = 20;
+    private Pane[] vaated;
+    private int hetkeVaade;
+    VeebiKlient klient;
+
+    @Override
+    public void start(Stage pealava) throws IOException {
+
+        //Loeme eelistused sätete failist.
+        Properties eelistused = Satted.loeSätted("app.properties");
+        int aknaKõrgus = Integer.parseInt(eelistused.getProperty("aken.korgus"));
+        int aknaLaius = Integer.parseInt(eelistused.getProperty("aken.laius"));
+        String stiil = this.getClass().getResource(eelistused.getProperty("stiil")).toExternalForm();
+
+        //Käivitame veebikliendi
+        System.setProperty("webdriver.chrome.silentOutput", "true");                     // peidab kasutaja jaoks ebaolulise info
+        java.util.logging.Logger.getLogger("org.openqa.selenium").setLevel(Level.SEVERE);   // Ei tekita üleliigset logi
+        WebDriverManager.chromedriver().setup();                                         //Tõmbab vajadusel alla chromedriveri kui seda ei eksisteeri
+        klient = new VeebiKlient();
+
+
+        hetkeVaade = 0;
+        VBox vaateHoidja = new VBox();
+        vaateHoidja.setPadding(new Insets(PADDING));
+        vaateHoidja.getStylesheets().add(stiil);
+
+        Scene stseen = new Scene(vaateHoidja, aknaKõrgus, aknaLaius);
+        vaated = new Pane[]{new VeebileheVaade(klient, stseen.heightProperty(), stseen.widthProperty())};
+
+        vaateHoidja.getChildren().add(vaated[hetkeVaade]);
+
+
+        pealava.setOnCloseRequest(e -> klient.close());
+        pealava.setScene(stseen);
+        pealava.show();
+
+
+    }
+
+    @Override
+    public void stop(){
+        klient.quit();
+    }
+}
---
+++ src/main/java/VeebileheVaade.java
@@ -0,0 +1,54 @@
+import ImageScraper.VeebiKlient;
+import javafx.beans.property.ReadOnlyDoubleProperty;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+public class VeebileheVaade extends VBox {
+    private VeebiKlient klient;
+
+    public VeebileheVaade(VeebiKlient klient, ReadOnlyDoubleProperty kõrgus, ReadOnlyDoubleProperty laius){
+        this.klient = klient;
+        Label veebilehtTekst = new Label("Veebileht");
+        TextField veebileheAadress = new TextField("");
+        HBox.setHgrow(veebileheAadress, Priority.ALWAYS);
+        Button otsiNupp = new Button("Otsi");
+        ImageView eelvaateVaade = new ImageView();
+        eelvaateVaade.fitWidthProperty().bind(laius.multiply(0.9));
+        eelvaateVaade.fitHeightProperty().bind(kõrgus.multiply(0.9));
+        otsiNupp.setOnMouseClicked(mouseEvent -> {
+                new Thread(() ->
+                {
+                    klient.get(veebileheAadress.getText());
+                    Image eelvaade = null;
+                    try {
+                        eelvaade = SwingFXUtils.toFXImage(klient.teeLehePilt(), null);
+                    } catch (IOException e) {
+                        //TODO create meaningful error
+                        e.printStackTrace();
+                    }
+                    eelvaateVaade.setImage(eelvaade);
+                }).start();
+
+        });
+
+        HBox sisend = new HBox(15, veebilehtTekst, veebileheAadress, otsiNupp);
+        sisend.setAlignment(Pos.CENTER);
+
+
+        this.getChildren().addAll(sisend, eelvaateVaade);
+        this.setAlignment(Pos.CENTER);
+        this.spacingProperty().set(20);
+        System.out.println(this.getHeight());
+    }
+
+}
